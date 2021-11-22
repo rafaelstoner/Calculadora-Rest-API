315a2347549be8350d9fc0e67aa158c77b8af7cf
Rafael Stoner Manuel Naiene <rafaelstoner7@gmail.com>
2021-11-22 23:45:06 +0200

    Doc: Atualização de ficheiros de configuração
    Adicionadas novas dependêncis necessárias para o funcionamento da app

--- rest/src/main/java/com/example/rest/RestApplication.java
+++ rest/src/main/java/com/example/rest/RestApplication.java
@@ -1,5 +1,8 @@
 package com.example.rest;
 
+import com.example.calculadora.handler.HandlerDeMensagens;
+import com.example.calculadora.handler.ImplementacaoHandlerDeMensagens;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +12,7 @@ public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
+
+
 
 }
